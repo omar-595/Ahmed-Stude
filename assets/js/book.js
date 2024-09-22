55bf66444d19e86ae82ca6612571e3c923c85b87
omar ahmed <omr39629@gmail.com>
2024-09-22 22:18:08 +0300

    Add files via upload

---
+++ assets/js/book.js
@@ -0,0 +1,59 @@
+function sho(ve) {
+	var id_a = document.getElementById('bo1');
+	var id_b = document.getElementById('button-1');
+	var id_c = document.getElementById('button-2');
+
+	id_b.style.display = "none";
+	id_c.style.display = "block";
+	id_a.style.display = "block";
+}
+
+function hid(ve) {
+	var id_a = document.getElementById('bo1');
+	var id_b = document.getElementById('button-1');
+	var id_c = document.getElementById('button-2');
+
+	id_b.style.display = "block";
+	id_c.style.display = "none";
+	id_a.style.display = "none";
+}
+// ******************************
+function sho2(ve) {
+	var id_a = document.getElementById('bo2');
+	var id_b = document.getElementById('button-3');
+	var id_c = document.getElementById('button-4');
+
+	id_b.style.display = "none";
+	id_c.style.display = "block";
+	id_a.style.display = "block";
+}
+
+function hid2(ve) {
+	var id_a = document.getElementById('bo2');
+	var id_b = document.getElementById('button-3');
+	var id_c = document.getElementById('button-4');
+
+	id_b.style.display = "block";
+	id_c.style.display = "none";
+	id_a.style.display = "none";
+}
+// ******************************************
+function sho3(ve) {
+	var id_a = document.getElementById('bo3');
+	var id_b = document.getElementById('button-5');
+	var id_c = document.getElementById('button-6');
+
+	id_b.style.display = "none";
+	id_c.style.display = "block";
+	id_a.style.display = "block";
+}
+
+function hid3(ve) {
+	var id_a = document.getElementById('bo3');
+	var id_b = document.getElementById('button-5');
+	var id_c = document.getElementById('button-6');
+
+	id_b.style.display = "block";
+	id_c.style.display = "none";
+	id_a.style.display = "none";
+}
